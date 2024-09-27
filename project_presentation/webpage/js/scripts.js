0adf3a72f774509091eb43440ceb97c20e903da2
theeduke <oscarian528@gmail.com>
2024-09-27 07:57:07 -0500

    simple webpage

---
+++ project_presentation/webpage/js/scripts.js
@@ -0,0 +1,8 @@
+// function hideStyles() {
+//         document.getElementById('nail-styles').classList.add('hidden');
+//     }
+    
+function openAppointmentPage() {
+        window.location.href = "appointment.html";
+    }
+
